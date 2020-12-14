e639d528b02c617d2cba4717cd7e0c1256f13a10
QuantumCookie <csksethu111@gmail.com>
2020-12-15 00:14:23 +0530

    Added rabbit eye highlight behaviour Added cursor change on tool selection

--- Hyperpharm/MainWindow.xaml.cs
+++ Hyperpharm/MainWindow.xaml.cs
@@ -15,14 +15,69 @@ using System.Windows.Shapes;
 
 namespace Hyperpharm
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Tool activeTool;
+
+        private Cursor cursorDefault;
+        private Cursor cursorTorch;
+        private Cursor cursorCotton;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            activeTool = Tool.NONE;
+
+            cursorDefault = Cursors.Arrow;
+            cursorCotton = new Cursor(Application.GetResourceStream(
+            new Uri("res/cursor_cotton.cur", UriKind.Relative)).Stream
+            );
+            cursorTorch = new Cursor(Application.GetResourceStream(
+            new Uri("res/cursor_torch.cur", UriKind.Relative)).Stream
+            );
+
+            toolTorch.Click += TorchButtonClick;
+            toolCotton.Click += CottonButtonClick;
+        }
+
+        private void ToolClicked(Tool tool)
+        {
+            if(tool == activeTool)
+            {
+                this.Cursor = cursorDefault;
+                activeTool = Tool.NONE;
+                return;
+            }
+            else
+            {
+                activeTool = tool;
+            }
+
+            switch(activeTool)
+            {
+                case Tool.TORCH:
+                    this.Cursor = cursorTorch;
+                    break;
+
+                case Tool.COTTON:
+                    this.Cursor = cursorCotton;
+                    break;
+
+                default:
+                    this.Cursor = cursorDefault;
+                    break;
+            }
+        }
+
+        private void TorchButtonClick(object sender, RoutedEventArgs e)
+        {
+            ToolClicked(Tool.TORCH);
+        }
+
+        private void CottonButtonClick(object sender, RoutedEventArgs e)
+        {
+            ToolClicked(Tool.COTTON);
         }
     }
 }
